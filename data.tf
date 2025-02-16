a70463ab5d2f1a365b154ed7e22490b1cb21f0b3
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-02-16 15:08:23 +0530

    terraform-vpc-module

--- data.tf
+++ data.tf
@@ -9,3 +9,10 @@ data "aws_vpc" "default" {
   
 }
 
+data "aws_route_table" "main" {
+    vpc_id = local.default_vpc.id
+    filter {
+      name = "association.main"
+      values = ["true"]
+    }
+}
